3fd0ce0e1f7689774d65f6fd6dc49eb137a98d6c
nitsolution2021 <praveen@nitsolution.in>
2021-12-14 13:44:21 +0530

    addee project

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+//create abn array of objects
+salesPersonList: SalesPerson[] = [
+  new SalesPerson("Anup","Kumar","anupkumar@gmail.com",50000),
+  new SalesPerson("Praveen","Kumar","praveenKumar@gmail.com",60000),
+  new SalesPerson("Archana","Kumari","archanakumari@gmail.com",70000),
+  new SalesPerson("abc","abc","abc@gmail.com",80000)
+];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
